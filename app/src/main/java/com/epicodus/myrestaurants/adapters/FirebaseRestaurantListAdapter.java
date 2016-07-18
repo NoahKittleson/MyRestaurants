ed4cd0f53ad02d145c81bd8c5ab9914d9f0c899c
Kyle <wolfsonk@gmail.com>
2016-07-18 10:49:57 -0700

    added functions for deleting and reordering saved restaurants

---
+++ app/src/main/java/com/epicodus/myrestaurants/adapters/FirebaseRestaurantListAdapter.java
@@ -0,0 +1,7 @@
+package com.epicodus.myrestaurants.adapters;
+
+/**
+ * Created by Guest on 7/18/16.
+ */
+public class FirebaseRestaurantListAdapter {
+}
---
+++ app/src/main/java/com/epicodus/myrestaurants/util/SimpleItemTouchHelperCallback.java
@@ -0,0 +1,7 @@
+package com.epicodus.myrestaurants.util;
+
+/**
+ * Created by Guest on 7/18/16.
+ */
+public class SimpleItemTouchHelperCallback {
+}
